7d17f7249b3e3861174a78f7def576ae983f57b6
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2024-04-03 11:09:59 +0100

    Added in Compose UI and refactored location

--- app/src/main/java/uk/ac/warwick/cim/aiinstreet/MainActivity.kt
+++ app/src/main/java/uk/ac/warwick/cim/aiinstreet/MainActivity.kt
@@ -1,28 +1,39 @@
 package uk.ac.warwick.cim.aiinstreet
 
 import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationResult
-import android.content.Context
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-
+import com.google.android.gms.location.LocationServices
 import uk.ac.warwick.cim.aiinstreet.ui.theme.AIinStreetTheme
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
 
 class MainActivity : ComponentActivity() {
+
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
@@ -30,9 +41,14 @@ class MainActivity : ComponentActivity() {
     private lateinit var locationRequest: LocationRequest
 
     // globally declare LocationCallback
-    protected lateinit var locationCallback: LocationCallback
+    private lateinit var locationCallback: LocationCallback
 
-    private lateinit var context: Context
+    private var requestingLocationUpdates = false
+
+    private var locationText: String? = null
+
+    private var locationUrl: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,42 +59,130 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    //@todo: write this to file and check if points exist.
+                    //val url = ""
+                    //getPoints(url);
 
-                    fusedLocationClient = LocationServices.getFusedLocationProviderClient(context!!)
-                    Greeting("Android")
+                    val locationPermissionRequest = registerForActivityResult(
+                        ActivityResultContracts.RequestMultiplePermissions()
+                    ) { permissions ->
+                        when {
+                            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                                // Precise location access granted.
+                            }
+                            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                                // Only approximate location access granted.
+                            } else -> {
+                            // No location access granted.
+                        }
+                        }
+                    }
+                    locationPermissionRequest.launch(arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION))
+
+                    if (ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        requestingLocationUpdates = true
+                    }
+                    fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location : Location? ->
+                            // Got last known location. In some rare situations this can be null.
+                        }
+
+                    locationCallback = object : LocationCallback() {
+
+                        override fun onLocationResult(locationResult: LocationResult) {
+                            locationResult ?: return
+                            locationText = ""
+                            for (location in locationResult.locations){
+                                // Update UI with location data
+                                // ...
+
+                                /*
+                                * if (long, lat)
+                                * If locatoin matches test
+                                 */
+                                locationText += "- @lat: ${location.latitude}\n" +
+                                        "- @lng: ${location.longitude}\n"
+                            }
+                        }
+                    }
+                    AudioMessage(name = locationText, url = locationUrl)
                 }
             }
         }
     }
 
+
     /**
-     * call this method in onCreate
-     * onLocationResult call when location is changed
+     * AudioMessage is a changeable view object.
+     *
+     * By default, we have a standard message that shows.
+     * When the URL is filled in, a button is shown to play the audio.
      */
-    private fun getLocationUpdates()
-    {
+    @Composable
+    fun AudioMessage(name:String?, url:String?){
+        Spacer(modifier = Modifier.width(8.dp))
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context!!)
-        locationRequest = LocationRequest()
-        locationRequest.interval = 50000
-        locationRequest.fastestInterval = 50000
-        locationRequest.smallestDisplacement = 170f // 170 m = 0.1 mile
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //set according to your app function
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                locationResult ?: return
+        Column {
+            Text(
+                text ="$name",
+                color = MaterialTheme.colorScheme.secondary
+            )
 
-                if (locationResult.locations.isNotEmpty()) {
-                    // get latest location
-                    val location = locationResult.lastLocation
-                    //lcatoin distanceTo ?
-                    // use your location object
-                    // get latitude , longitude and other info from this
-                }
-
-
-            }
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text="$url")
         }
+
+        if (url != "") {
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Button(onClick = { /*TODO*/ }) { }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewAudioMessage(){
+        AIinStreetTheme {
+            AudioMessage(name = "Here lies static", url = "")
+        }
+    }
+    fun getPoints(url: String): String {
+        val connection = URL(url).openConnection()
+        val reader = BufferedReader(InputStreamReader(connection.getInputStream()))
+        val jsonData = StringBuilder()
+
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            jsonData.append(line)
+        }
+        reader.close()
+
+        //val jsonObject = JSONObject(jsonData)
+        return jsonData.toString()
+    }
+
+    /**
+     * If close to an audio way point, play audio.
+     */
+    fun playAudio(locationResult: LocationResult) {
+        Distance().distanceTo(locationResult)
     }
 
     //start location updates
@@ -98,14 +202,15 @@ class MainActivity : ComponentActivity() {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+            requestingLocationUpdates = true
             return
         }
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            null /* Looper */
-        )
-    }
+            fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper())
+        }
+
+
 
     // stop location updates
     private fun stopLocationUpdates() {
@@ -113,7 +218,7 @@ class MainActivity : ComponentActivity() {
     }
 
     // stop receiving location update when activity not visible/foreground
-    fun onPause() {
+    override fun onPause() {
         super.onPause()
         stopLocationUpdates()
     }
@@ -121,22 +226,8 @@ class MainActivity : ComponentActivity() {
     // start receiving location update when activity  visible/foreground
     override fun onResume() {
         super.onResume()
-        startLocationUpdates()
+        if (requestingLocationUpdates) startLocationUpdates()
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AIinStreetTheme {
-        Greeting("Android")
-    }
 }
