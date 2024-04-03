2bb35088c006e66eaa5e4b75437d8b159d99b899
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2024-04-03 11:28:15 +0100

    Added media player

---
+++ app/src/main/java/uk/ac/warwick/cim/aiinstreet/AudioPlayer.kt
@@ -0,0 +1,35 @@
+package uk.ac.warwick.cim.aiinstreet
+
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+
+class AudioPlayer {
+    private lateinit var mediaPlayer: MediaPlayer
+    fun play(url:String) {
+        val url = "http://........" // your URL here
+         mediaPlayer = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+            setDataSource(url)
+            prepare() // might take long! (for buffering, etc)
+            start()
+        }
+    }
+
+    fun stop () {
+        mediaPlayer?.release()
+        //mediaPlayer = null
+    }
+
+    fun pause() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+        } else {
+            mediaPlayer.start()
+        }
+    }
+}
