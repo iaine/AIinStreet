60b7810b6d74010f81a0251a5310a551e3e0aa45
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2024-04-08 17:56:57 +0100

    Basic unit tests for distance

--- app/src/test/java/uk/ac/warwick/cim/aiinstreet/DistanceTest.kt
+++ app/src/test/java/uk/ac/warwick/cim/aiinstreet/DistanceTest.kt
@@ -2,25 +2,34 @@ package uk.ac.warwick.cim.aiinstreet
 
 import android.location.Location
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class DistanceTest {
 
+    @Mock
+    private lateinit var testLocation1: Location
+
+    @Mock
+    private lateinit var testLocation2: Location
     @Test
     fun testDistanceIsNear() {
         val dist = Distance()
-        val testLocation = Location("newLocation")
-        testLocation.latitude = 52.410826
-        testLocation.longitude = -1.522640
-        assert(dist.distanceTo(testLocation))
+        Mockito.`when`(testLocation1.latitude).thenReturn(52.410826)
+        Mockito.`when`(testLocation1.longitude).thenReturn(-1.522640)
+        assert(dist.distanceTo(testLocation1))
     }
 
     @Test
     fun testDistanceIsFar() {
         val dist = Distance()
-        val testLocation = Location("testLocation")
-        testLocation.latitude = 52.410826
-        testLocation.longitude = -1.522640
-        assert(!dist.distanceTo(testLocation))
+        Mockito.`when`(testLocation2.latitude).thenReturn(53.410979)
+        Mockito.`when`(testLocation2.longitude).thenReturn(-2.522789)
+
+        assert(dist.distanceTo(testLocation2))
     }
 
     @Test
