62b3a027c5bb63d2d6a436fd53d4ae46b4a3de1c
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2024-04-18 17:46:20 +0100

    Updated audio and text functions

--- app/src/main/java/uk/ac/warwick/cim/aiinstreet/Distance.kt
+++ app/src/main/java/uk/ac/warwick/cim/aiinstreet/Distance.kt
@@ -1,6 +1,7 @@
 package uk.ac.warwick.cim.aiinstreet
 
 import android.location.Location
+import android.location.LocationManager
 import kotlinx.serialization.json.Json
 import java.io.File
 
@@ -14,22 +15,34 @@ class Distance {
 
         val latitude = 52.410825
         val longitude = -1.522639
-        val newLocation = Location("")
+        val newLocation = Location(LocationManager.GPS_PROVIDER)
         newLocation.latitude = latitude
         newLocation.longitude = longitude
+        val results = FloatArray(1)
 
-        if (location.distanceTo(newLocation) < 2.5) {
+        Location.distanceBetween(location.latitude, location.longitude, newLocation.latitude, newLocation.longitude, results)
+        if (results[0] < 2.5) {
             near = true
         }
         return near
     }
 
-    fun locationAudio(lat: Double, long: Double):String {
-        return "http://blag.com"
+    fun locationAudio(lat: Double, long: Double, locations:MutableList<AudioLocations>):String {
+        var url = ""
+        val filtered = this.findLocations(locations, lat, long)
+        if (filtered.size == 1) {
+            url = filtered[0].audioUrl
+        }
+        return url
     }
 
-    fun locationText (lat: Double, long: Double): String {
-        return "set text"
+    fun locationText (lat: Double, long: Double, locations:MutableList<AudioLocations>): String {
+        var text = ""
+        val filtered = this.findLocations(locations, lat, long)
+        if (filtered.size == 1) {
+            text = filtered[0].audioText
+        }
+        return text
     }
 
     private fun getLocations (): AudioLocations {
@@ -37,5 +50,9 @@ class Distance {
         val file = File(filePath)
         val locationJSON = file.readText()
         return Json.decodeFromString<AudioLocations>(locationJSON)
+    }
+
+    fun findLocations (aLocation: MutableList<AudioLocations>, lat: Double, long: Double): List<AudioLocations> {
+        return aLocation.filter{it.lat == lat && it.long == long}
     }
 }
